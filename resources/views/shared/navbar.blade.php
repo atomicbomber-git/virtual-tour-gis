28acb4520699433f29528d4bb675dbde3d456c28
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-01-17 09:35:55 +0700

    Updated navbar.

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -6,6 +6,12 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
+                <li class='nav-item {{ Route::is('layer.*') ? 'active' : '' }}'>
+                    <a class='nav-link' href='{{ route('layer.index') }}'>
+                        <i class='fa fa-list-alt'></i>
+                        Layer
+                    </a>
+                </li>
             </div>
         </div>
     </div>
