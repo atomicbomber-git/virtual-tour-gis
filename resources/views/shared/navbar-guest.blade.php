743f19ecb68ff79c3ec51843b7e5aa8ac2a89049
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-05-09 18:27:18 +0700

    Updated.

--- resources/views/shared/navbar-guest.blade.php
+++ resources/views/shared/navbar-guest.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-default fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html"><i class="fa fa-map"></i> Sistem Informasi Geografis & Virtual Tour <br> Wisata Kabupaten Kapuas Hulu</a>
+        <a class="navbar-brand" href="index.html"><i class="fa fa-map mr-2"></i> Sistem Informasi Geografis & Virtual Tour Wisata Kabupaten Kapuas Hulu</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
