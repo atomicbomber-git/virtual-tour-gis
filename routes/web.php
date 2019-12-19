bd16fdd35d8d1a2fafab59e1bc13950e9a842d06
Antonius Yonathan <atomicbomber.email@gmail.com>
2019-12-19 16:26:40 +0700

    Minor fix.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\GuestVirtualTourController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
